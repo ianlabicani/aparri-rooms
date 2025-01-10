6b9198817cd12853c24df4493ca8db6ddeae3623
ianlabicani <labicaniian@gmail.com>
2025-01-10 19:06:51 +0800

    create initial UI for landlord dashboard

---
+++ resources/views/landlord/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('landlord.shell')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Landlord Dashboard</h1>
+                <p>Welcome to the Landlord Dashboard</p>
+            </div>
+        </div>
+    </div>
+@endsection
