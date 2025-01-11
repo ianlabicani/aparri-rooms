cf042226bf64749d06ad26497d1fd2bba2346568
ianlabicani <labicaniian@gmail.com>
2025-01-11 11:50:50 +0800

    add routes for room management and images in landlord section

--- routes/landlord.php
+++ routes/landlord.php
@@ -1,8 +1,15 @@
 <?php
+use App\Http\Controllers\Landlord\RoomController;
+use App\Http\Controllers\Landlord\RoomImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix("landlord")->group(function () {
+Route::prefix("landlord")->name('landlord.')->middleware(['auth', 'role:landlord'])->group(function () {
   Route::get('dashboard', function () {
     return view('landlord.dashboard.index');
-  })->name('landlord.dashboard');
+  })->name('dashboard');
+
+  // rooms resource
+  Route::resource('rooms', RoomController::class);
+  Route::resource('room-images', RoomImageController::class);
+
 });
