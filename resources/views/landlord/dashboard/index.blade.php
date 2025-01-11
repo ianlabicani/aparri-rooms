0a3ed994611a48c36d7a3d84ac6e94c51c528e72
ianlabicani <labicaniian@gmail.com>
2025-01-11 11:49:50 +0800

    add dashboard page UI

--- resources/views/landlord/dashboard/index.blade.php
+++ resources/views/landlord/dashboard/index.blade.php
@@ -1,12 +1,10 @@
 @extends('landlord.shell')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>Landlord Dashboard</h1>
-                <p>Welcome to the Landlord Dashboard</p>
-            </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Landlord Dashboard</h1>
+            <p>Welcome to the Landlord Dashboard</p>
         </div>
     </div>
 @endsection
